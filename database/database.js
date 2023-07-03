24932ce6700a0a4dc533d2fc5cc15a1e2a69b996
Raik silva <raiksilva@gmail.com>
2023-07-03 02:12:39 -0300

    conexão banco

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const connection = new Sequelize("guia_perguntas", 'root', 'Senha',{
+const connection = new Sequelize("guia_perguntas", 'root', 'R@ik1423',{
     host: 'localhost',
     port: 5012,
     dialect: 'mysql'
