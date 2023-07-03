0c09e24fc86a97f547dbd8d545c01a125d703e17
Raik silva <108504214+Raiksilva@users.noreply.github.com>
2023-07-03 01:51:09 -0300

    atualizando tabela para não receber valor nulo

--- models/Resposta.js
+++ models/Resposta.js
@@ -4,16 +4,25 @@ const connection = require("../database/database");
 const Resposta = connection.define('resposta',{
     corpo: {
         type: Sequelize.TEXT,
-        allowNull: false
+        validate: {
+            notEmpty: {
+              msg: 'O campo "corpo" não pode estar em branco.'
+            }
+        }
     },
     perguntaId: {
         type: Sequelize.INTEGER,
-        allowNull: false
+        allowNull: false,
+        validate: {
+            notEmpty: {
+              msg: 'O campo "pergunta Id" não pode estar em branco.'
+            }
+        }
     }
 });
 
 Resposta.sync({force: false}).then(() =>{
-    console.log("tabela 2 sincronizada")
+    
 });
 
 module.exports = Resposta;
